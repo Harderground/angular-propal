c23259b5264749c148b221e34e4944b49d4a3689
Harderground <34180764+Harderground@users.noreply.github.com>
2018-06-03 16:55:21 -0400

    no message

--- src/app/components/despacho/despacho.component.ts
+++ src/app/components/despacho/despacho.component.ts
@@ -9,44 +9,49 @@ declare var $: any;
   styleUrls: ['./despacho.component.css']
 })
 export class DespachoComponent implements OnInit {
-  public id: any =1;
+  public id: any = 1;
   public camiones: any[] = [];
   public camionesCargas: any[] = [];
   public camionesHistoricos: any[] = [];
+  public camionesFiltro: any[] = [];
+  public camionesCargasFiltro: any[] = [];
+  public camionesHistoricosFiltro: any[] = [];
   public despachar = "active";
   public anden;
   public despachados;
-  public texto:any="";
+  public texto: any = "";
   constructor(private _despachoService: DespachoService, private router: ActivatedRoute) {
-   }
+  }
 
   ngOnInit() {
 
     this.obtenerCamionesDespachar();
     this.obtenerCamionesEnCarga();
     this.obtenerCamionesHistorico();
-    
+
   }
-  obtenerCamionesHistorico(){
-    this._despachoService.getCamionesHistorico().subscribe( 
-        result => {
-            console.log(result);
-            this.camionesHistoricos = result;
-        },
-        error =>  {
-            console.log(error);
-        }
+  obtenerCamionesHistorico() {
+    this._despachoService.getCamionesHistorico().subscribe(
+      result => {
+        console.log(result);
+        this.camionesHistoricos = result;
+        this.camionesHistoricosFiltro = result;
+      },
+      error => {
+        console.log(error);
+      }
     );
   }
-  obtenerCamionesEnCarga(){
-    this._despachoService.getCamionesEnCarga().subscribe( 
-        result => {
-            console.log(result);
-            this.camionesCargas = result;
-        },
-        error =>  {
-            console.log(error);
-        }
+  obtenerCamionesEnCarga() {
+    this._despachoService.getCamionesEnCarga().subscribe(
+      result => {
+        console.log(result);
+        this.camionesCargas = result;
+        this.camionesCargasFiltro = result;
+      },
+      error => {
+        console.log(error);
+      }
     );
   }
   obtenerCamionesDespachar() {
@@ -54,12 +59,45 @@ export class DespachoComponent implements OnInit {
       result => {
         console.log(result);
         this.camiones = result;
+        this.camionesFiltro = result;
       },
       error => {
         console.log(error);
       }
     );
   }
+
+  filtroCamion(a) {
+    for (let i = 0; this.camionesFiltro.length; i++) {
+      if (this.camionesFiltro[i].res_puerto == null) {
+        this.camionesFiltro[i].res_puerto = "";
+      }
+      if( this.camionesFiltro[i].res_puerto == undefined){
+        this.camionesFiltro[i].res_puerto = "";
+      }
+    }
+    let filtro: any[] = this.camionesFiltro.filter(filter =>
+      (
+        /* filter.num_regPort.toString().indexOf(a.target.value.toString()) != -1 ||
+         filter.C_Fecha.toString().toUpperCase().indexOf(a.target.value.toUpperCase()) != -1 ||
+         filter.nom_tipoVehiculo.toString().toUpperCase().indexOf(a.target.value.toUpperCase()) != -1 ||
+         filter.patente.toString().toUpperCase().indexOf(a.target.value.toUpperCase()) != -1 ||
+         filter.nom_chofer.toString().toUpperCase().indexOf(a.target.value.toUpperCase()) != -1 ||
+         (filter.nro_contenedor + "").toString().toUpperCase().indexOf(a.target.value.toUpperCase()) != -1 ||*/
+        filter.res_puerto.toString().indexOf(a.target.value.toUpperCase()) ||
+        filter.ord_carga_descarga.toString().toUpperCase().indexOf(a.target.value.toUpperCase()) != -1 ||
+        filter.nom_agTransp.toUpperCase().indexOf(a.target.value.toUpperCase()) != -1), error => { console.log(error) });
+    console.log(filtro);
+    if (a.target.value == "") {
+      this.obtenerCamionesDespachar();
+    } else if (filtro.length != 0) {
+      this.camiones = filtro;
+    }
+  }
+
+
+
+
   setMenu(id: any) {
     switch (id) {
       case 1: {
